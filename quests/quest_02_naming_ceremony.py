788397a9c9c7b8c9378599506a5417d8760f3a63
root <root@ph4nt0m.localdomain>
2026-06-26 18:36:08 +0200

    repos

---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+hero_name = "Shadow Knight"
+superpower = "controlling shadows"
+
+print(f"{hero_name} is a brave hero with the power of {superpower}.")
---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def calculate_area(length, width):
+    return length * width
+
+
+rectangle1 = calculate_area(5, 10)
+rectangle2 = calculate_area(7, 3)
+
+print(f"The area of rectangle 1 is {rectangle1}.")
+print(f"The area of rectangle 2 is {rectangle2}.")

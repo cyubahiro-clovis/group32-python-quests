c1a9c2e9bb9bb9783d66d0a026984c17964452a2
aanyar1-tech <a.anyar1@alustudent.com>
2026-06-27 04:56:23 -0700

    Add quests 9, 14, 20, 24 - Member 6

---
+++ quests/quest_09_greedy_goblin.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+gold = 27
+friends = 4
+each = gold // friends
+remainder = gold % friends
+print("Each friend gets {} gold pieces.".format(each))
+print("The goblin keeps {} gold pieces.".format(remainder))

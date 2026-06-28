5dfb654742ce5d4eeb72690ed70ae0bc10030197
Vladimir-Ndayisaba <v.ndayisaba@alustudent.com>
2026-06-28 20:13:58 +0200

    Vladimir

--- quests/quest_06_fortune_teller.py
+++ quests/quest_06_fortune_teller.py
@@ -4,3 +4,4 @@ name = input("What is your name? ")
 quest = input("What is your quest? ")
 
 print(f"Welcome, {name}! Your quest is: {quest}. May fortune guide you!")
+# Member 3 work by Vladimir-Ndayisaba
